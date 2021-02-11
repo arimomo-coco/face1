d235a1e9fe42247f4c6da4d7175480440909bb13
arimomo <coyn0123@gmail.com>
2021-02-12 01:27:57 +0900

    tell the time

--- src/c/face1.c
+++ src/c/face1.c
@@ -7,6 +7,8 @@ static void init();
 static void deinit();
 static void main_window_load(Window* window);
 static void main_window_unload(Window* window);
+static void tick_handler(struct tm* tick_time, TimeUnits units_changed);
+static void update_time();
 
 //declare variables
 static Window* s_main_window;
@@ -30,6 +32,12 @@ static void init(){
 
 	//Show the Window on the watch, with animated = true
 	window_stack_push(s_main_window, true);
+
+	//Make sure the time is displayed from the start
+	update_time();
+
+	//Register with TickTimerService
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(){
@@ -59,4 +67,21 @@ static void main_window_load(Window* window){
 static void main_window_unload(Window* window){
 	//Destroy TextLayer
 	text_layer_destroy(s_time_layer);
+}
+
+static void tick_handler(struct tm* tick_time, TimeUnits units_changed){
+	update_time();
+}
+
+static void update_time(){
+	//Get a tm structure
+	time_t temp = time(NULL);
+	struct tm* tick_time = localtime(&temp);
+
+	//Write the current hours and minutes into a buffer
+	static char s_buffer[8];
+	strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style()? "%H:%M": "%I:%M", tick_time);
+
+	//Display this time on the TextLayer
+	text_layer_set_text(s_time_layer, s_buffer);
 }
